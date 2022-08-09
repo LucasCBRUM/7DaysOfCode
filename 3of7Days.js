47193e1b2365c1a05f3527354dfb23662b9c8d44
Lucas Brum <lucas09cbrum@gmail.com>
2022-08-09 12:40:20 -0300

    Fix the style
    Increment max height and width

--- 3of7Days.js
+++ 3of7Days.js
@@ -4,7 +4,7 @@ let front = '<br><a href="http://www.google.com"><div id="Front"><img src="https
 '<a href="http://www.google.com"><div id="Back"><img src="https://img.freepik.com/premium-vector/back-end-word-concepts-banner-database-programming-web-applications-development-presentation-website-isolated-lettering-typography-idea-with-linear-icons-vector-outline-illustration_106317-8775.jpg?w=740"></div></a>';
 
 let back = `<h1>Que incrivel, Back-end é show!</h1><h1>Dentro do Back-end entre essas duas linguagens, qual você escolheria?</h1><section class="flex-container"><button value="front" onclick="BackTwoStep()"id="Front"><div><img src="https://static.cloud-boxloja.com/lojas/wyfyg/produtos/e3a5cbf1-f38e-4b2d-9263-d2216fd778f0.jpg"></div></button>
-<button href="http://www.google.com"><div id="Back"><img src="https://img.freepik.com/premium-vector/back-end-word-concepts-banner-database-programming-web-applications-development-presentation-website-isolated-lettering-typography-idea-with-linear-icons-vector-outline-illustration_106317-8775.jpg?w=740"></div></button></section>`;
+<button><div id="Back"><img src="https://mpng.subpng.com/20181122/krs/kisspng-java-programming-language-selenium-computer-softwa-july-2-16-halab-4-dev-5bf78387a7bb41.028192901542947719687.jpg" style="margin-left:-5rem;"></div></button></section>`;
 
 
 function frontStep() {
